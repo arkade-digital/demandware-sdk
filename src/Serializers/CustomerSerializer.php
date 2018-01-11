d963ababcfc6a87a1d340300da3afb8f590da6f2
Andy Johnston <andy@arkade.com.au>
2018-01-11 16:19:48 +1100

    customer serializer for create and update

--- src/Serializers/CustomerSerializer.php
+++ src/Serializers/CustomerSerializer.php
@@ -17,6 +17,31 @@ class CustomerSerializer
         // trigger the entities jsonSerialize method
         $serialized = json_decode(json_encode($customer));
 
+        // demandware fields are in snake case
+        $vars = get_object_vars($serialized);
+        $serialized = [];
+        foreach ($vars as $key => $value){
+            $serialized[snake_case($key)] = $value;
+        }
+
+        // convert credentials fields to snake case
+        if(isset($serialized['credentials'])){
+            $vars = get_object_vars($serialized['credentials']);
+            $serialized['credentials'] = [];
+            foreach ($vars as $key => $value){
+                $serialized['credentials'][snake_case($key)] = $value;
+            }
+
+            unset($serialized['credentials']['locked']);
+            unset($serialized['credentials']['enabled']);
+        }
+
+        // strip null value items
+        $serialized = array_filter($serialized);
+
+        // remove id field
+        unset($serialized['id']);
+
         return json_encode($serialized);
     }
 }

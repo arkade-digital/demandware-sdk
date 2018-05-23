0ec806c80dd982166ab34423f848d23dd8b17166
Andy Johnston <andy@arkade.com.au>
2018-05-24 08:24:29 +1000

    only update customer address if address id is existing

--- src/Modules/Customers.php
+++ src/Modules/Customers.php
@@ -110,13 +110,15 @@ Class Customers Extends AbstractModule
 
         $customerNo = $customerResponse->getCustomerNo();
         if ($customer->getPrimaryAddress()) {
-            $this->client->patch(
-                $this->useData("customer_lists/{$this->getSiteName()}/customers/{$customerNo}/addresses/{$customer->getPrimaryAddress()->getAddressId()}"),
-                [
-                    'headers' => ['Content-Type' => 'application/json'],
-                    'body'    => (new AddressSerializer)->serialize($customer->getPrimaryAddress()),
-                ]
-            );
+            if($customer->getPrimaryAddress()->getAddressId()) {
+                $this->client->patch(
+                    $this->useData("customer_lists/{$this->getSiteName()}/customers/{$customerNo}/addresses/{$customer->getPrimaryAddress()->getAddressId()}"),
+                    [
+                        'headers' => ['Content-Type' => 'application/json'],
+                        'body' => (new AddressSerializer)->serialize($customer->getPrimaryAddress()),
+                    ]
+                );
+            }
         }
 
         return $this->findById($customerNo);
